8d1c9068b693a0299746e833178b405faced2072
Thi-Vo-Mia <162828859+Thi-Vo-Mia@users.noreply.github.com>
2025-03-22 23:44:26 +0000

    connect sql connection w interface

--- LancasterMusicHall/src/Database/SQLConnection.java
+++ LancasterMusicHall/src/Database/SQLConnection.java
@@ -1,4 +1,15 @@
 package Database;
 
-public class SQLConnection {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class SQLConnection implements SQLInterface {
+    // Database URL and credentials for connection.
+    private String url = "jdbc:mysql://sst-stuproj.city.ac.uk:3306/xxx";
+    // In a real application, use secure methods to manage database credentials.
+    private String dbUser = "your_db_username";
+    private String dbPassword = "your_db_password";
 }
