049642e12bc2b3929d3031ba698dea6b3e55ecf2
Baqader01 <mysarahsb@gmail.com>
2025-03-23 03:26:04 +0000

    Finished

---
+++ LancasterMusicHall/src/GUI/MenuPanels/EventPanel.java
@@ -0,0 +1,11 @@
+package GUI.MenuPanels;
+
+import GUI.MainMenuGUI;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class EventPanel extends JPanel {
+    public EventPanel(MainMenuGUI mainMenu) {
+    }
+}
