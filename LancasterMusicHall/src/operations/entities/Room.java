09a2532aa52c119eaf58e0dd06204ce29a216c28
Thi-Vo-Mia <162828859+Thi-Vo-Mia@users.noreply.github.com>
2025-04-07 02:05:28 +0100

    update room

--- LancasterMusicHall/src/operations/entities/Room.java
+++ LancasterMusicHall/src/operations/entities/Room.java
@@ -1,9 +1,70 @@
 package operations.entities;
 
 public class Room {
-    private int roomId;              // ID field
-    private String roomNumber;
-    private int maxOccupancy;
+    private String roomName;
+    private int venueId;
+    private Integer roomNumber;
+    private Integer roomCapacity;
+    private int classroomCapacity;
+    private int boardroomCapacity;
+    private int presentationCapacity;
+    private String seatingType;
 
-    // Constructors, Getters, Setters...
+    public Room(String roomName, int venueId, Integer roomNumber, Integer roomCapacity,
+                int classroomCapacity, int boardroomCapacity, int presentationCapacity, String seatingType) {
+        this.roomName = roomName;
+        this.venueId = venueId;
+        this.roomNumber = roomNumber;
+        this.roomCapacity = roomCapacity;
+        this.classroomCapacity = classroomCapacity;
+        this.boardroomCapacity = boardroomCapacity;
+        this.presentationCapacity = presentationCapacity;
+        this.seatingType = seatingType;
+    }
+
+    public String getRoomName() {
+        return roomName;
+    }
+
+    public int getVenueId() {
+        return venueId;
+    }
+
+    public Integer getRoomNumber() {
+        return roomNumber;
+    }
+
+    public Integer getRoomCapacity() {
+        return roomCapacity;
+    }
+
+    public int getClassroomCapacity() {
+        return classroomCapacity;
+    }
+
+    public int getBoardroomCapacity() {
+        return boardroomCapacity;
+    }
+
+    public int getPresentationCapacity() {
+        return presentationCapacity;
+    }
+
+    public String getSeatingType() {
+        return seatingType;
+    }
+
+    @Override
+    public String toString() {
+        return "Room{" +
+                "roomName='" + roomName + '\'' +
+                ", venueId=" + venueId +
+                ", roomNumber=" + roomNumber +
+                ", roomCapacity=" + roomCapacity +
+                ", classroomCapacity=" + classroomCapacity +
+                ", boardroomCapacity=" + boardroomCapacity +
+                ", presentationCapacity=" + presentationCapacity +
+                ", seatingType='" + seatingType + '\'' +
+                '}';
+    }
 }
